433abcfedd3887be01567a300c9f9d2f1d7c4743
devruchoa <ronaldo.uchoa.pessoal@gmail.com>
2023-10-15 10:32:20 -0300

    feat: create NotificationService

---
+++ src/main/java/com/uchoa/picpaysimplificado/service/NotificationService.java
@@ -0,0 +1,26 @@
+package com.uchoa.picpaysimplificado.service;
+
+import com.uchoa.picpaysimplificado.domain.user.User;
+import com.uchoa.picpaysimplificado.dto.NotificationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NotificationService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String message) throws Exception {
+        String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("http://o4d9z.mocklab.io/notify", notificationRequest, String.class);
+
+        if (!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
+            System.out.println("Error sending notification");
+            throw new Exception("Error sending notification");
+        }
+    }
+}
