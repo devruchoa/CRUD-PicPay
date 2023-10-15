cd424acd46198eda60df9874396182dfb4ba3cb0
devruchoa <ronaldo.uchoa.pessoal@gmail.com>
2023-10-15 10:31:43 -0300

    feat: create ControllerExceptionHandler

---
+++ src/main/java/com/uchoa/picpaysimplificado/infra/ControllerExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.uchoa.picpaysimplificado.infra;
+
+import com.uchoa.picpaysimplificado.dto.ExceptionDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Usuário já cadastrado", "400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Usuário não encontrado", "404");
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(Exception exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+}
